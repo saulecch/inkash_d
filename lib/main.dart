5ef00b4d81053ff26bf1b1556b8aa7dd486a8534
Saúl Calderón <saulecc@gmail.com>
2026-07-30 12:10:20 -0600

    S05 estructura de hero lista

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: Scaffold(
         body: ListView(
+          padding: EdgeInsets.all(16),
           children: [
             Row(
               mainAxisAlignment: .spaceBetween,
               children: [Text('Hola, Kevin'), Text('Julio 2026')],
             ),
+            SizedBox(height: 20),
             Column(
+              crossAxisAlignment: .start,
               children: [
                 Text('TE QUEDAN DISPONIBLES', style: TextStyle(fontSize: 10)),
+                Text('Q.2976.50', style: TextStyle(fontSize: 50)),
+                LinearProgressIndicator(value: 0.57),
+                Text('Has usado Q.3,7...', style: TextStyle(fontSize: 12)),
               ],
             ),
           ],
